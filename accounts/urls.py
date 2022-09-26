037bb1327ef2e2d6173c00824f16adb147f28ed8
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-09-26 12:41:51 +0530

    Templates/Media/Apps/db setup

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+urlpatterns = [
+    # path('signup/', views.signup),
+    # path('login/',views.login),
+    # path('logout/', views.login),
+    # path('profile/', views.profile),
+    # path('stats/', views.login)
+]
