01b8b3b46e40e4d533229f4e007cca7aec23c16c
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-10-08 18:30:25 +0530

    accounts finishing touches done

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from . import models
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = models.Profile
-        exclude = ['user','following','followers']
+        exclude = ['user','following','followers','phone']
 
         widgets = {
             'bday' : forms.DateInput(attrs={'type':'date'})
